3be6bfb47c7dedce82b3f78ccaf23772913c3a99
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2025-11-25 22:32:40 +0530

    Add files via upload

---
+++ Week1/Day1/Day2/03-function.js
@@ -0,0 +1,32 @@
+//Named Function
+
+// function funName(){
+
+//     console.log("Hello Team Welcome");
+// }
+
+// funName()
+
+//Call a function to execute the code inside the function block
+
+// function funNameAdd(a,b){ // FUnction definition (parameters)
+// // let a =10;
+// // let b =20;
+// let c = a+b
+// console.log(c); 
+ 
+// }
+
+// funNameAdd(10,20)//passing arguments while calling the function
+
+//call a function to execute the code inside the function block/scopw and  return the value
+
+function funNameAddRe(a,b){ // FUnction definition (parameters)
+// //return keyword means the funNameAdd(a,b)=c=a+b
+// let a =10;
+// let b =20;
+let c = a+b
+return c  
+}
+
+console.log(funNameAddRe(10,20))//passing arguments while calling the function
